93a8078614779500de3451c3fb31512e961ba231
keansharmsac <98183147+keansharmsac@users.noreply.github.com>
2022-02-10 17:23:02 -0500

    Add files via upload

---
+++ Multiply.cs
@@ -0,0 +1,11 @@
+﻿namespace MyMultiplyLib;
+public class Multiply
+{
+    public static int Multi(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public static int Multi(int num1, int num2, int num3){
+        return num1 * num2 * num3;
+    }
+}
